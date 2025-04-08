7facf00ea0d94b18262e7252611520224c3e18cc
Bao44 <tqbao44@gmail.com>
2025-04-08 17:00:40 +0700

    add docker

---
+++ microservice/gateway-service/src/main/java/vn/edu/iuh/fit/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
